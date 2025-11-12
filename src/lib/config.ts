6f80efba7027fdd611542d1d6f184564dc36fd4b
Adrino Araujo <adrianoaraujodsantos@hotmail.com>
2025-11-11 21:43:53 -0300

    chore: update github repo name

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,6 @@ export const THEMES = ["system", "light", "dark"] as const;
 export type Theme = (typeof THEMES)[number];
 
 export const GITHUB_OWNER = "adrianoaraujods";
-export const GITHUB_REPO = "calc2";
+export const GITHUB_REPO = "calculatex";
 export const GITHUB_BRANCH = "master";
 export const GITHUB_URL = `https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}`;
